67bd433ba1de3b8e956570865e97c5d23ae07eba
Julio E <je27@protonmail.com>
2020-04-14 20:39:44 -0700

    assignment 2 finished

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import execute_values
 import numpy as np
-​
+
 psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
-​
+
 load_dotenv() #> loads contents of the .env file into the script's environment
-​
+
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
@@ -17,7 +17,7 @@ DB_HOST = os.getenv("DB_HOST")
 # READ CSV
 #​
 # if your CSV file is in the "data" dir:
-CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "data", "titanic.csv")
+CSV_FILEPATH = os.path.join(os.path.dirname(__file__),"titanic.csv")
 df = pandas.read_csv(CSV_FILEPATH)
 df.index += 1
 print(df.head())
@@ -25,22 +25,12 @@ print(df.head())
 # CONNECT TO PG DATABASE
 #
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-print(type(connection)) #> <class 'psycopg2.extensions.connection'>
 
 cursor = connection.cursor()
-print(type(cursor)) #> <class 'psycopg2.extensions.cursor'>​
-#query = "SELECT * from test_table;"
-#cursor.execute(query)
-#results = cursor.fetchall()
-#print(type(results)) #> list
-#print(results)
-​
 #
 # CREATE TABLE
 #
-​
 table_creation_query = """
-DROP TABLE passengers;
 CREATE TABLE IF NOT EXISTS passengers (
   id SERIAL PRIMARY KEY,
   survived integer,
@@ -54,25 +44,15 @@ CREATE TABLE IF NOT EXISTS passengers (
 );
 """
 cursor.execute(table_creation_query)
-​
 #
 # INSERT DATA IN THE TABLE
 #
-​
-#list_of_tuples = [
-#  ('A rowwwww', 'null'),
-#  ('Another row, with JSONNNNN', json.dumps(my_dict)),
-#  ('Third row', "3")
-#]
-​
-# how to convert dataframe into a list of tuples?
-​
+
 list_of_tuples = list(df.to_records(index=True))
 # possibly sometimes would need to do further transformations, perhaps using a list comprehension
-​
 insertion_query = "INSERT INTO passengers (id, survived, pclass, name, gender, age, sib_spouse_count, parent_child_count, fare) VALUES %s"
 execute_values(cursor, insertion_query, list_of_tuples)
-​
+
 # SAVE THE RESULTS!
 connection.commit()
 # CLEAN UP!
